ec129997712b75ef9dbdb8d044d667c7f2b49e90
Mallory <mallory.feuer@gmail.com>
2019-06-19 20:41:48 -0400

    add devise to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
      resources :users, only: [:index, :create, :destroy]
      get '/stocks/:ticker/:qty', to: 'stocks#index'
 
+     root to: "home#index"
 
 
 
