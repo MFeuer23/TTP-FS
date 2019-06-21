3b75ba6088ae58970c80d9607fdb694bf5fd2a6f
Mallory <mallory.feuer@gmail.com>
2019-06-20 21:05:00 -0400

    drop pages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_003444) do
+ActiveRecord::Schema.define(version: 2019_06_21_010419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "favorites", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "category"
-    t.string "source_url"
-  end
 
   create_table "stocks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,15 +26,11 @@ ActiveRecord::Schema.define(version: 2019_06_20_003444) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
