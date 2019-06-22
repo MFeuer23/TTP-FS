23b0597b4ae8f38b337d5cdd13fda0240b0b5412
Mallory <mallory.feuer@gmail.com>
2019-06-22 01:42:08 -0400

    display values qty * current price to two decimal places

--- app/javascript/components/Stock.js
+++ app/javascript/components/Stock.js
@@ -35,7 +35,7 @@ class Stock extends Component {
   render(){
     return (
       <div className={this.stockColor()}>
-        {this.props.ticker.toUpperCase()} - {this.props.qty} Shares - ${this.state.qty * this.state.current_price}
+        {this.props.ticker.toUpperCase()} - {this.props.qty} Shares - ${(this.props.qty * this.state.current_price).toFixed(2)}
       </div>
     )
   }
