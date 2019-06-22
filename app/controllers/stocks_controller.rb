16b92183d319deb525f7527e56eeed6edbc9b749
Mallory <mallory.feuer@gmail.com>
2019-06-21 22:28:28 -0400

    create user, stock, and trade when user buys a stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,13 @@
 class StocksController < ApplicationController
 
   def index
-    
+
+  end
+
+  def create
+
+    @user = User.find(params[:current_user][:id])
+    @stock = Stock.create(ticker_symbol: params[:ticker])
+    @trade = Trade.create(user_id: @user.id, stock_id: @stock.id, transaction_price: params[:stockData][:current_price], qty: params[:qty])
   end
 end
