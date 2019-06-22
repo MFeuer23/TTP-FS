8539f9cb7ae3a5f69223791d532bd22fdbbc3b8a
Mallory <mallory.feuer@gmail.com>
2019-06-21 23:55:15 -0400

    render stocks with classes set based on difference between current and open price

--- app/javascript/components/Stock.js
+++ app/javascript/components/Stock.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react'
 
+class Stock extends Component {
+  constructor() {
+    super();
+    this.state = {
+      open_price: 0,
+      current_price: 0
+    }
+  }
 
-function Stock(props) {
-  return (
-    <div className="Stock">
-      "Stock"
-    </div>
-  );
+  componentDidMount = () => {
+    fetch(`https://cloud.iexapis.com/stable/stock/${this.props.ticker}/quote?token=pk_b1a1b59742544768ba38683c68c5337b`)
+      .then((res) => { return res.json() })
+      .then((data) => { return this.setState(
+
+          {current_price: data.latestPrice,
+          open_price: data.open}
+
+      ); }
+    )
+      .catch((err) => { console.log(err) })
+  }
+
+  stockColor = () => {
+    if (this.state.open_price > this.state.current_price) {
+      return "red"
+    } else if (this.state.open_price < this.state.current_price) {
+      return "green"
+    } else {
+      return "grey"
+    }
+  }
+
+  render(){
+    return (
+      <div className={this.stockColor()}>
+        {this.props.ticker}
+      </div>
+    )
+  }
+
 }
 
 export default Stock;
