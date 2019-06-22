881ebf2a13f4898f0d2b9369e79c41ad7c2cb7ca
Mallory <mallory.feuer@gmail.com>
2019-06-21 23:54:21 -0400

    change to find or create by instead of create to guard against duplicate table rows

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,7 +6,7 @@ class StocksController < ApplicationController
 
   def create
     @user = User.find(params[:current_user][:id])
-    @stock = Stock.create(ticker_symbol: params[:ticker])
+    @stock = Stock.find_or_create_by(ticker_symbol: params[:ticker])
     @trade = Trade.create(user_id: @user.id, stock_id: @stock.id, transaction_price: params[:stockData][:current_price], qty: params[:qty])
   end
 end
