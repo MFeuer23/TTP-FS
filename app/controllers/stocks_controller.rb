a4084578fe5f69281a27748e455c504aedf8e8bc
Mallory <mallory.feuer@gmail.com>
2019-06-23 16:43:39 -0400

    only create trade if user has enough money

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,7 +9,6 @@ class StocksController < ApplicationController
         @stocks[Stock.find(t.stock_id)[:ticker_symbol]] = t.qty
       end
     end
-
     render json: @stocks
   end
 
@@ -18,8 +17,8 @@ class StocksController < ApplicationController
 
     @stock = Stock.find_or_create_by(ticker_symbol: params[:ticker])
 
-    @user = User.find(params[:current_user][:id])
-    @stock = Stock.find_or_create_by(ticker_symbol: params[:ticker])
-    @trade = Trade.create(user_id: @user.id, stock_id: @stock.id, transaction_price: params[:stockData][:current_price], qty: params[:qty], ticker: @stock.ticker_symbol)
+    if @user.cash >= (params[:stockData][:current_price] * params[:qty].to_i)
+      @trade = Trade.create(user_id: @user.id, stock_id: @stock.id, transaction_price: params[:stockData][:current_price], qty: params[:qty])
+    end
   end
 end
