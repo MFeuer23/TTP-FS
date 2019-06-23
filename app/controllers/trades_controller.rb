b07dbaccd5e7f669588f51b749d9a2c1ec6c60bf
Mallory <mallory.feuer@gmail.com>
2019-06-23 15:27:48 -0400

    delete unused create action

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -4,10 +4,4 @@ class TradesController < ApplicationController
     render json: current_user.trades
   end
 
-  def create
-    
-    @user = User.find(params[:current_user][:id])
-    @stock = Stock.find_or_create_by(ticker_symbol: params[:ticker])
-    @trade = Trade.create(user_id: @user.id, stock_id: @stock.id, transaction_price: params[:stockData][:current_price], qty: params[:qty], ticker: @stock.ticker_symbol)
-  end
 end
