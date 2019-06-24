83d21c32034ace17d31a4c362d33bbe6ffcbf968
Mallory <mallory.feuer@gmail.com>
2019-06-23 23:26:56 -0400

    made custom registration controller for devise, requires name attribute at signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :trades
   has_many :stocks, through: :trades
 
+  validates :name, presence: true
+  validates :email, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: [:index, :show, :update, :create, :destroy]
 
      root to: "home#index"
