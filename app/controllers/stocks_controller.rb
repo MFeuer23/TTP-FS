c8dee621a8604b78f616f381172ed71b95f6dd68
Mallory <mallory.feuer@gmail.com>
2019-06-23 23:25:20 -0400

    create trades with ticker attribute

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -18,7 +18,7 @@ class StocksController < ApplicationController
     @stock = Stock.find_or_create_by(ticker_symbol: params[:ticker])
 
     if @user.cash >= (params[:stockData][:current_price] * params[:qty].to_i)
-      @trade = Trade.create(user_id: @user.id, stock_id: @stock.id, transaction_price: params[:stockData][:current_price], qty: params[:qty])
+      @trade = Trade.create(user_id: @user.id, stock_id: @stock.id, transaction_price: params[:stockData][:current_price], qty: params[:qty], ticker: params[:ticker])
     end
   end
 end
